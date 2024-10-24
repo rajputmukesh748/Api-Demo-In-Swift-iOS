a40595a1a7d2ea36f23b473177b7a40896ba7cb0
Mukesh Kumar <mukesh.rajput@appinventiv.com>
2024-10-24 14:17:48 +0530

    Initial Commit

---
+++ Api Practice in Swift UI/Api_Practice_in_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_Practice_in_Swift_UIApp.swift
+//  Api Practice in Swift UI
+//
+//  Created by apple on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Api_Practice_in_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
